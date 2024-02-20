c7673c22b9d426e3a9ea9ed10ced3ccefebb9f0c
xlanglois <xavier.langlois@gmail.com>
2024-02-20 16:22:41 +0100

    start goroutine + channel

--- main.go
+++ main.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	// Import the function from the same directory
 	common "github.com/xl714/go-image-weight-reducer/common"
 	//filesys "github.com/xl714/go-image-weight-reducer/common"
 	imagehelper "github.com/xl714/go-image-weight-reducer/imagehelper"
 )
+
+type FileInfo struct {
+	Path   string
+	Name   string
+	Ext    string
+	IsDir  bool
+	Weight float64
+}
+
+type ProcessInfo struct {
+	Path string
+	SizeOriginal int64
+	IsResized bool
+	SizeNew int64
+	ErrorMessage string
+	DateUpdatedOriginal string
+	DateUpdatedNew string
+	DateCreatedOriginal string
+	DateCreatedNew string
+}
 
 func main() {
 	args, err := common.ParseArguments()
@@ -27,7 +48,12 @@ func main() {
 		fmt.Println("Error:", err)
 		return
 	}
+	
+	var wg sync.WaitGroup
+	processInfoChan := make(chan FileInfo)
+
 	fmt.Println("Image files:")
+
 	for i, file := range imageFiles {
 		if args.Limit > 0 && i >= args.Limit {
 			break
@@ -35,14 +61,45 @@ func main() {
 		fmt.Printf("Path: %s, Name: %s, Ext: %s, IsDir: %t, Weight: %.2f\n", file.Path, file.Name, file.Ext, file.IsDir, file.Weight)
 
 		if file.Weight > args.MaxWeight {
-			newPath, err := imagehelper.ResizeImage(file.Path, file.Ext, file.Weight, args.MaxWeight, args.Verbose)
-			if err != nil {
-				fmt.Println("    Error parsing arguments:", err)
-			} else {
-				fmt.Printf("   Image weight new path: %s\n", newPath)
-			}
+			fmt.Println("   => Add to process")
+			wg.Add(1)
+			go processFile(file, args.MaxWeight, args.Verbose, &wg, processInfoChan)	
+			// if err != nil {
+			// 	fmt.Println("    Error parsing arguments:", err)
+			// } else {
+			// 	fmt.Printf("   Image weight new path: %s\n", newPath)
+			// }
 		} else {
 			fmt.Println("   Image weight is less than max weight")
 		}
 	}
+	go func() {
+		wg.Wait()
+		close(processInfoChan)
+	}()
+	fmt.Println("All done infos")
+	for processInfo := range processInfoChan {
+		// fmt.Printf("File: %s, Size: %d bytes\n", fileInfo.Path, fileInfo.Size)
+		fmt.Printf("%+v\n", processInfo)
+	}
+}
+
+func processFile(file:FileInfo, maxWeight, verbose, wg *sync.WaitGroup, ch chan<- ProcessInfo) {
+	defer wg.Done()
+
+	newPath, err := imagehelper.ResizeImage(file.Path, file.Ext, file.Weight, maxWeight, verbose)
+
+	processInfo := ProcessInfo{
+        Path:                newPath,
+        SizeOriginal:        file.Weight,
+        IsResized:           true,
+        SizeNew:             0,
+        ErrorMessage:        err,
+        DateUpdatedOriginal: "2022-01-01",
+        DateUpdatedNew:      "2024-02-20",
+        DateCreatedOriginal: "2022-01-01",
+        DateCreatedNew:      "2024-02-20",
+    }
+
+	ch <- processInfo
 }
